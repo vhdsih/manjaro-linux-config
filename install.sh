c0bb3d157e141982c0f5a1d8ee091abbedfdfe63
橘子 <dongchangzhang@gmail.com>
2018-02-12 14:24:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if [ -d "$HOME/.manjaro-linux-config" ];
+then
+    mv $HOME/.manjaro-linux-config $HOME/.manjaro-linux-config.bak
+fi
+
 git clone https://github.com/dongchangzhang/manjaro-linux-config $HOME/.manjaro-linux-config
 cd $HOME/.manjaro-linux-config
 ./tools/setup.sh
